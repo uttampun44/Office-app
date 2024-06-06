42a8910347807a24950606110649175218b62362
uttam pun <uttampun62@gmail.com>
2024-06-06 21:23:20 +0545

    project setup

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside>
+    <div class="sidebar-panel h-full w-48  fixed top-0  bg-gray-800 px-4 py-8">
+         <div class="sidebar-title">
+              <h1 class="text-2xl font-bold text-white">Office Panel</h1>
+         </div>
+
+          <div class="sidebar-routes">
+              <ul>
+                <li><a class="text-white font-bold text-lg">Dashboard</a></li>
+                  <li><a class="text-white font-bold text-lg">Roles</a></li>
+                  <li><a class="text-white font-bold text-lg">permissions</a></li>
+              </ul>
+          </div>
+    </div>
+</aside>
